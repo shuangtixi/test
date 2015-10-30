12de5bcb82660604dcb2c6b2ff900391c86bbfea
zhangl <1541163472@qq.com>
2015-10-30 14:06:53 +0800

    u2

--- src/com/Hello.java
+++ src/com/Hello.java
@@ -3,6 +3,6 @@ package com;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("hello world update1...");
+		System.out.println("hello world update2222...");
 	}
 }
